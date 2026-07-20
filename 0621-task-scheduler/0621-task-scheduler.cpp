dc36f4328aef5863eca84ead80d909704dcd7b1c
utkarsh khasgiwale <utkarshkhasgiwale16@gmail.com>
2026-07-21 01:20:43 +0530

    Time: 1902 ms (5.01%), Space: 459.1 MB (5.01%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int gap) {
+        int n = tasks.size();
+         unordered_map<char, int> freq;
+         unordered_map<char, int> free;
+          priority_queue<pair<int, char>> pq;
+          int seat = 0;
+         
+          for(int i = 0; i < n; i++) {
+            freq[tasks[i]]++;
+            free[tasks[i]] = 1;
+            }
+
+          for(auto x : freq)
+           pq.push( {x.second, x.first} );
+
+          while(!pq.empty()){
+            vector<pair<int,char>> pulled;
+            while(!pq.empty()){
+                pair<int,char> p = pq.top();
+                pq.pop();
+
+                int times = p.first;
+                char letter = p.second;
+
+                if(free[letter] <= seat){
+
+                    if(p.first > 1) pq.push( {p.first-1, p.second} );
+
+                free[p.second] = seat + gap + 1;
+                break;
+
+                }
+
+                else pulled.push_back(p);
+            }
+            for(auto x : pulled) pq.push(x);
+            seat++;
+          }
+
+        return seat - 1;
+    }
+};
