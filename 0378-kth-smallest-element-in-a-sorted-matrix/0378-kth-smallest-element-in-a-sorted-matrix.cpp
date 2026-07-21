86073e5d9d366c8c8398e400ad4e582547f399e3
utkarsh khasgiwale <utkarshkhasgiwale16@gmail.com>
2026-07-21 10:49:57 +0530

    Time: 11 ms (24.67%), Space: 17.1 MB (80.7%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,43 +1,46 @@
 class Solution {
-public:
 
-    int Counter(vector<vector<int>>& table, int m, int n, int target){
-        int row = n -1;
-        int col = 0;
-        int count = 0;
+    class Node{
+        public:
+        int value;
+        int row;
+        int col;
 
-        while(row >= 0 && col < m){
-            if(table[row][col] <= target){
-               count += row + 1;
-               col++;
-            }
-            else{
-                row--;
-            }
-        }
-        return count;
+    Node(int v, int r, int c){
+        value = v;
+        row = r;
+        col = c;
     }
-
+    };
+    struct cmp{
+        bool operator()(const Node& a, const Node& b){
+            return a.value > b.value;
+        }
+    };
+    public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<Node, vector<Node>, cmp> pq;
         int n = matrix.size();
         int m = matrix[0].size();
+        int res = 0;
 
-        int low = matrix[0][0];
-        int high = matrix[n-1][m-1];
-
-        int res = -1;
-
-        while(low <= high){
-            int guess = (low + high) / 2;
-            int smallestElement = Counter(matrix, n, m, guess);
-            if(smallestElement < k){
-                low = guess + 1;
-            }
-            else{
-                res = guess;
-                high = guess - 1;
-            }
+        for(int i = 0; i < n; i++){
+            pq.push( {matrix[i][0], i, 0} );
         }
-        return res;
+
+        while(k--){
+            Node curr = pq.top();
+            pq.pop();
+
+            int v = curr.value;
+            int r = curr.row;
+            int c = curr.col;
+            
+            res = v;
+            if (c + 1 < m)
+            pq.push( {matrix[r][c + 1], r, c + 1} );
+        }
+
+       return res;
     }
 };
